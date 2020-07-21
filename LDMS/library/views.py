a63ad85bd500e597fe2ed4ff357804099979c632
Ahajuu <56596060+Ahajuu@users.noreply.github.com>
2020-07-21 12:27:45 +0530

    adding userform

--- LDMS/library/views.py
+++ LDMS/library/views.py
@@ -1,9 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import CreateUserForm
 
 # Create your views here.
 
 def index(request):
-    return render(request,'home/index.html')
+    form = CreateUserForm()
+    context = {'form':form}
+    if request.method == 'POST':  
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            email = form.cleaned_data.get('email')
+
+            print(user,email)
+            return redirect('home')
+        else:
+            print("epty")
+            
+            # context.update({"value":"fail"})
+            # return render(request,"designer/register.html",context)
+            #check whether its printing in console
+             
+            #messages.info(request,'Failed')
+       
+    
+    print(context)
+    
+    return render(request,'home/index.html',context)
 
 def home(request):
     return render(request,'home/section.html')
